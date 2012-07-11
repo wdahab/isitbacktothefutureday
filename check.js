47925a08da3948e7aae877fbd3bb8bc5ab996e2b
wdahab <wdahab@gmail.com>
2012-07-11 16:43:21 -0400

    basic

---
+++ check.js
@@ -0,0 +1,19 @@
+body {
+	background: white;
+	color: black;
+}
+
+.content {
+	margin: 200px auto 0 auto;
+	color: black;
+}
+
+h2 {
+	font-size: 40px;
+	font-family: serif;
+}
+
+p {
+	font-size: 20px;
+	font-family: sans-serif;
+}
